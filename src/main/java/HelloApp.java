41347a1d8cfb66effb864b4466061cde7357e12e
himesh <hp7735@srmist.edu.in>
2026-03-18 10:48:48 +0530

    UC1: print Hello World message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,17 @@
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("hello");
+
+    public static void main(String[] args) {
+
+        // Check if a name is provided as a command-line argument
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is given
+            System.out.println("Hello, World!");
+        }
+
     }
+
 }
+
