95f6422fdf8c4b44721f16787f1a941b2fc5bccf
himesh <hp7735@srmist.edu.in>
2026-03-18 11:04:13 +0530

    UC1: print Hello World message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,12 @@
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("hello");
+
+    public static void main(String[] args) {
+
+        // Assign name using ternary operator (default = "World")
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
     }
+
 }
